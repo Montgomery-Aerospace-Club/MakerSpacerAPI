4e8321f404575fb3a4b5acd84a6868d9428f9f48
ed.xyz <eddietang2314@gmail.com>
2023-08-17 18:37:40 -0400

    change migrations

--- mventory/settings.py
+++ mventory/settings.py
@@ -14,6 +14,7 @@ import os
 
 from pathlib import Path
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,85 +25,84 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('MVENTORY_SECRET_KEY')
+SECRET_KEY = os.getenv("MVENTORY_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = eval(os.getenv('MVENTORY_DEBUG')) or False
+DEBUG = eval(os.getenv("MVENTORY_DEBUG")) or False
 
-ALLOWED_HOSTS = [os.getenv('MVENTORY_HOSTNAME')]
+ALLOWED_HOSTS = [os.getenv("MVENTORY_HOSTNAME"), "localhost"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django_prometheus',
-    'rest_framework',
-    'inventory.apps.InventoryConfig'
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django_prometheus",
+    "rest_framework",
+    "inventory.apps.InventoryConfig",
 ]
 
 MIDDLEWARE = [
-    'django_prometheus.middleware.PrometheusBeforeMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_prometheus.middleware.PrometheusAfterMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-
+    "django_prometheus.middleware.PrometheusBeforeMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_prometheus.middleware.PrometheusAfterMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
-ROOT_URLCONF = 'mventory.urls'
+ROOT_URLCONF = "mventory.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'mventory.wsgi.application'
+WSGI_APPLICATION = "mventory.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 # Get Database Settings from the Environment
-db_engine_name = os.getenv('MVENTORY_DB_ENGINE') or "sqlite3"
+db_engine_name = os.getenv("MVENTORY_DB_ENGINE") or "sqlite3"
 DB_ENGINE = f"django_prometheus.db.backends.{db_engine_name}"
 
 if db_engine_name == "sqlite3":
     DATABASES = {
-        'default': {
-            'ENGINE': 'django_prometheus.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'data/mventory.sqlite3',
+        "default": {
+            "ENGINE": "django_prometheus.db.backends.sqlite3",
+            "NAME": BASE_DIR / "data/mventory.sqlite3",
         }
     }
 else:
     DATABASES = {
-        'default': {
-            'ENGINE': DB_ENGINE,
-            'NAME': os.getenv('MVENTORY_DB_NAME'),
-            'HOST': os.getenv('MVENTORY_DB_HOST'),
-            'USER': os.getenv('MVENTORY_DB_USER'),
-            'PASSWORD': os.getenv('MVENTORY_DB_PASSWORD'),
+        "default": {
+            "ENGINE": DB_ENGINE,
+            "NAME": os.getenv("MVENTORY_DB_NAME"),
+            "HOST": os.getenv("MVENTORY_DB_HOST"),
+            "USER": os.getenv("MVENTORY_DB_USER"),
+            "PASSWORD": os.getenv("MVENTORY_DB_PASSWORD"),
         }
     }
 
@@ -112,16 +112,16 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -129,9 +129,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-gb'
+LANGUAGE_CODE = "en-gb"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -142,26 +142,22 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "/static/"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # REST Framework
-REST_FRAMEWORK = {
-            'TEST_REQUEST_DEFAULT_FORMAT': 'json'
-            }
+REST_FRAMEWORK = {"TEST_REQUEST_DEFAULT_FORMAT": "json"}
 
 # Octopart
-OCTOPART_API_KEY=os.getenv('MVENTORY_OCTOPART_API_KEY')
-OCTOPART_API_ENDPOINT='https://octopart.com/api/v4/endpoint'
+OCTOPART_API_KEY = os.getenv("MVENTORY_OCTOPART_API_KEY")
+OCTOPART_API_ENDPOINT = "https://octopart.com/api/v4/endpoint"
--- mventory/urls.py
+++ mventory/urls.py
@@ -21,18 +21,18 @@ from rest_framework import routers
 from inventory import views
 
 router = routers.DefaultRouter()
-router.register(r'buildings', views.BuildingViewSet)
-router.register(r'rooms', views.RoomViewSet)
-router.register(r'storage_units', views.StorageUnitViewSet)
-router.register(r'storage_bins', views.StorageBinViewSet)
-router.register(r'components', views.ComponentViewSet)
-router.register(r'component_measurements', views.ComponentMeasurementUnitViewSet)
+router.register(r"buildings", views.BuildingViewSet)
+router.register(r"rooms", views.RoomViewSet)
+router.register(r"storage_units", views.StorageUnitViewSet)
+router.register(r"storage_bins", views.StorageBinViewSet)
+router.register(r"components", views.ComponentViewSet)
+router.register(r"component_measurements", views.ComponentMeasurementUnitViewSet)
+router.register(r"users", views.UserViewSet)
 
 urlpatterns = [
-    path('', views.index),
-    path('admin/', admin.site.urls),
-    path('rest/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('', include('django_prometheus.urls'))
-
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+    path("", views.index),
+    path("admin/", admin.site.urls),
+    path("rest/", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("", include("django_prometheus.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
