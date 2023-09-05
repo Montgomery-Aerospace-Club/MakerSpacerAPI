8acc90b54ef47bea61a3f21e9a3f76a1c4e6bf37
ed.xyz <eddietang2314@gmail.com>
2023-09-05 10:21:55 -0400

    added export option for nearly all models

--- inventory/admin.py
+++ inventory/admin.py
@@ -12,11 +12,55 @@ from .models import (
     Borrow,
 )
 
+from import_export.admin import ExportActionMixin
+
+
+class BuildingAdmin(ExportActionMixin, admin.ModelAdmin):
+    list_display = ("name", "address", "postcode")
+
+
+class RoomAdmin(ExportActionMixin, admin.ModelAdmin):
+    list_display = ("name", "short_code", "building")
+
+
+class StorageUnitAdmin(ExportActionMixin, admin.ModelAdmin):
+    list_display = ("name", "short_code", "room")
+
+
+class StorageBinAdmin(ExportActionMixin, admin.ModelAdmin):
+    list_display = ("name", "short_code", "unit_row", "unit_column", "storage_unit")
+
+
+class ComponentAdmin(ExportActionMixin, admin.ModelAdmin):
+    list_display = (
+        "name",
+        "sku",
+        "mpn",
+        "upc",
+        # "storage_bin",
+        "measurement_unit",
+        "qty",
+        "description",
+        "barcode",
+    )
+
+
+class BorrowAdmin(ExportActionMixin, admin.ModelAdmin):
+    list_display = (
+        "qty",
+        "person_who_borrowed",
+        "timestamp_check_out",
+        "timestamp_check_in",
+        "borrow_in_progress",
+        "component",
+    )
+
+
 admin.site.register(ComponentMeasurementUnit)
-admin.site.register(Building)
-admin.site.register(Room)
-admin.site.register(StorageUnit)
-admin.site.register(StorageBin)
-admin.site.register(Component)
+admin.site.register(Building, BuildingAdmin)
+admin.site.register(Room, RoomAdmin)
+admin.site.register(StorageUnit, StorageUnitAdmin)
+admin.site.register(StorageBin, StorageBinAdmin)
+admin.site.register(Component, ComponentAdmin)
 admin.site.register(User)
-admin.site.register(Borrow)
+admin.site.register(Borrow, BorrowAdmin)
--- inventory/models.py
+++ inventory/models.py
@@ -19,6 +19,9 @@ from django.core.files import File
 import os
 
 
+from import_export import resources
+
+
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
